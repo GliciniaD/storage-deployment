c96303689c3c42559c490d751bfba07770937f16
GD <glicinia95@gmail.com>
2026-05-02 21:49:08 +0100

    correct reference of rg to storage

--- storage.tf
+++ storage.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg-ado" {
+resource "azurerm_resource_group" "rg" {
   name     = "rg-ado-uks-01"
   location = "UK South"
 }
 
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "storage" {
   name                     = "sastorageexample"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
