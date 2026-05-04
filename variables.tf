f50dbfead6f2f739450e7537bb853e0e8f15bcb6
GD <glicinia95@gmail.com>
2026-05-04 13:02:19 +0100

    ADO errors

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
-variable "storage accounts" {
+variable "storage_accounts" {
   type        = map(object({
     rg_name = string
     storage_name = string
-    description = "name of storage account"
 }))
 }
